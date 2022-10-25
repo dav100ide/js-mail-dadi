faead6fe1fc161804c57b0d14b07410e8af67b63
dav100ide <oreglio.dev@gmail.com>
2022-10-25 16:51:31 +0200

    .innerHTML dyi

---
+++ dadi/js/main.js
@@ -0,0 +1,31 @@
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Che ci sia un array da qualche parte?
+// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+// Consigli del giorno:
+// 1. scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+// 2. javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+// 3. si ma noi cosa vogliamo fare?
+// 4. torniamo a scrivere in italiano
+// 5. proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+
+// genero un numero per il pc e per l'user
+
+const cpuNumber = Math.floor(Math.random() * 6) + 1;
+const userNumber = Math.floor(Math.random() * 6) + 1;
+document.getElementById('cpu-number-box').innerHTML = `computer: ${cpuNumber}`;
+document.getElementById('user-number-box').innerHTML = `tu: ${userNumber}`;
+
+const btnResult = document.getElementById('btn-result');
+btnResult.addEventListener('click', function () {
+   let result = document.getElementById('result').innerHTML;
+   if (cpuNumber > userNumber) {
+      result = 'ha vinto il computer';
+   } else if (userNumber > cpuNumber) {
+      result = 'hai vinto tu!';
+   } else {
+      result = 'pareggio, i numeri sono uguali';
+   }
+});
+console.log(result);
--- mail/js/main.js
+++ mail/js/main.js
@@ -3,14 +3,14 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-const userMail = document.getElementById('user-email');
 const validMails = ['pippo@gmail.com', 'marco@gmail.com', 'ciao@gmail.com', 'boolean@gmail.com'];
 const verifyBtn = document.getElementById('verify-mail');
 verifyBtn.addEventListener('click', function () {
+   const userMail = document.getElementById('user-email').value;
    for (let i = 0; i < validMails.length; i++) {
       let message = document.getElementById('message');
       const validMail = validMails[i];
-      if (userMail.value === validMail) {
+      if (userMail === validMail) {
          message.innerHTML = 'la mail è valida.';
          break;
       } else {
