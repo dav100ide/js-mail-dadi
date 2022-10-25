7d61cd6fbf79fa092807be1c809bdfa454908ed9
dav100ide <oreglio.dev@gmail.com>
2022-10-25 15:36:50 +0200

    togli console.log

--- mail/js/main.js
+++ mail/js/main.js
@@ -7,7 +7,6 @@ const userMail = document.getElementById('user-email');
 const validMails = ['pippo@gmail.com', 'marco@gmail.com', 'ciao@gmail.com', 'boolean@gmail.com'];
 const verifyBtn = document.getElementById('verify-mail');
 verifyBtn.addEventListener('click', function () {
-   console.log(userMail.value);
    for (let i = 0; i < validMails.length; i++) {
       let message = document.getElementById('message');
       const validMail = validMails[i];
@@ -17,6 +16,5 @@ verifyBtn.addEventListener('click', function () {
       } else {
          message.innerHTML = 'la mail NON è valida.';
       }
-      console.log(message);
    }
 });
