5bede48c035edb78fbf86c3476e0753c02ac8a9e
dav100ide <oreglio.dev@gmail.com>
2022-10-25 15:04:19 +0200

    eventlistener input?

--- mail/js/main.js
+++ mail/js/main.js
@@ -3,14 +3,15 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-const userMail = document.getElementById('user-mail').value;
-const validMails = ['pippo@gmail.com', 'marco@gmail,com', 'ciao@gmail.com', 'boolean@gmail.com'];
+const userMail = document.getElementById('user-email').value;
+const validMails = ['pippo@gmail.com', 'marco@gmail.com', 'ciao@gmail.com', 'boolean@gmail.com'];
 const verifyBtn = document.getElementById('verify-mail');
-
-verifyBtn.addEventListener('click', function () {
-   if ((userMail = validMails)) {
-      console.log('mail valida');
-   } else {
-      console.log('mail non valida');
-   }
-});
+console.log(userMail);
+// verifyBtn.addEventListener('click', function () {
+//    const validMail = validMails[i];
+//    if (userMail === validMail[i]) {
+//       console.log('ok');
+//    } else {
+//       console.log('non valida');
+//    }
+// });
