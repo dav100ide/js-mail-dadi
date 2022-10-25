3b19214254a38ad53d83f56d9c98d26041af2b77
dav100ide <oreglio.dev@gmail.com>
2022-10-25 15:36:04 +0200

    fine mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -3,14 +3,20 @@
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
 
-const userMail = document.getElementById('user-email').value;
+const userMail = document.getElementById('user-email');
 const validMails = ['pippo@gmail.com', 'marco@gmail.com', 'ciao@gmail.com', 'boolean@gmail.com'];
 const verifyBtn = document.getElementById('verify-mail');
 verifyBtn.addEventListener('click', function () {
-   console.log(userMail);
-   if (userMail === validMails) {
-      console.log('ok');
-   } else {
-      console.log('non valida');
+   console.log(userMail.value);
+   for (let i = 0; i < validMails.length; i++) {
+      let message = document.getElementById('message');
+      const validMail = validMails[i];
+      if (userMail.value === validMail) {
+         message.innerHTML = 'la mail è valida.';
+         break;
+      } else {
+         message.innerHTML = 'la mail NON è valida.';
+      }
+      console.log(message);
    }
 });
